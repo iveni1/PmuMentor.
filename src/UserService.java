e3fbbba5ddc3020513517c6df697d62da354c6bf
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 23:07:47 +0300

    Add files via upload

---
+++ src/UserService.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
+
+public class UserService {
+    protected Map<String, User> users = new HashMap<>();
+
+    public void createUser(String username, String password, String email) {
+        users.put(username, new User(username, password, email));
+    }
+
+    public boolean login(String username, String password) {
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
+    }
+
+    public void updateProfile(String username, String newEmail) {
+        User user = users.get(username);
+        if (user != null) {
+            user.setEmail(newEmail);
+        }
+    }
+
+    public void deleteUser(String username) {
+        users.remove(username);
+    }
+
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+}
